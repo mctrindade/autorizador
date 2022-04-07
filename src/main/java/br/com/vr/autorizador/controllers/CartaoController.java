130fc94d0422b1bbb5ff0d5d7447fa3594cb9081
Marcos Diniz <marcos.diniz@sicoob.com.br>
2022-04-07 20:30:35 -0300

    configuração do swagger das API's

--- src/main/java/br/com/vr/autorizador/controllers/CartaoController.java
+++ src/main/java/br/com/vr/autorizador/controllers/CartaoController.java
@@ -3,6 +3,7 @@ package br.com.vr.autorizador.controllers;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.vr.autorizador.dto.CartaoDTO;
 import br.com.vr.autorizador.services.CartaoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/cartoes")
 public class CartaoController {
-	
+
 	@Autowired
 	private CartaoService cartaoService;
-	
-	@PostMapping
+
+	@ApiOperation(value = "Cria um cartão com saldo de 500")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Criação com sucesso"),
+			@ApiResponse(code = 422, message = "Caso o cartão já exista") })
+	@PostMapping(produces = "application/json", consumes = "application/json")
 	public ResponseEntity<CartaoDTO> create(@RequestBody @Validated CartaoDTO cartaoDto) {
 		CartaoDTO cartaoNovoDto = cartaoService.create(cartaoDto);
-		return ResponseEntity.ok(cartaoNovoDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cartaoNovoDto);
 	}
 	
-	@GetMapping("{numeroCartao}")
-	public ResponseEntity<BigDecimal> getSaldo(@PathVariable String numeroCartao){
+	
+	@ApiOperation(value = "Recupera o saldo disponivel do cartão")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Obtenção com sucesso"),
+			@ApiResponse(code = 404, message = "Caso o cartão já exista") })
+	@GetMapping(path = "{numeroCartao}", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> getSaldo(@PathVariable String numeroCartao) {
 		BigDecimal saldo = cartaoService.getSaldo(numeroCartao);
 		return ResponseEntity.ok(saldo);
 	}
--- src/main/java/br/com/vr/autorizador/controllers/TransacaoController.java
+++ src/main/java/br/com/vr/autorizador/controllers/TransacaoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.vr.autorizador.dto.TransacaoDTO;
 import br.com.vr.autorizador.services.TransacaoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/transacoes")
@@ -16,8 +19,11 @@ public class TransacaoController {
 	
 	@Autowired
 	private TransacaoService transacaoService;
-		
-	@PostMapping
+	
+	@ApiOperation(value = "Realiza uma transação")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Transação realizada com sucesso"),
+			@ApiResponse(code = 422, message = "SALDO_INSUFICIENTE|SENHA_INVALIDA|CARTAO_INEXISTENTE") })
+	@PostMapping(produces="application/json", consumes="application/json" )
 	public ResponseEntity<String> transaction (@RequestBody TransacaoDTO transacaoDto){
 		transacaoService.realizarTransacao(transacaoDto);
 		return ResponseEntity.ok("Ok");
